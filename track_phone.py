01d23c5b032edeec5cf33f5a8dd67abf506dc824
mukesh kumar <57214574+MUKESHKUMAR2001@users.noreply.github.com>
2021-02-26 14:07:16 +0530

    Add files via upload

---
+++ track_phone.py
@@ -0,0 +1,20 @@
+import phonenumbers
+from phonenumbers import timezone
+from phonenumbers import carrier
+number = input("Enter a phone number along with country code: ")
+from phonenumbers import geocoder
+ch_number = phonenumbers.parse(number, "CH")
+print("-------------------------------------")
+print("    Phone-number:", number)
+print("         Country:", geocoder.description_for_number(ch_number, "en"))
+service_number = phonenumbers.parse(number, "RO")
+print("Service-Provider:", carrier.name_for_number(service_number, "en"))
+gb_number = phonenumbers.parse(number, "GB")
+print("        TimeZone:", timezone.time_zones_for_number(gb_number))
+parse_phone = phonenumbers.parse(number, None)
+print("  Parsing_phn_no:", parse_phone)
+is_possible_no = phonenumbers.is_possible_number(parse_phone)
+print("     Is-Possible:", is_possible_no)
+is_valid_no = phonenumbers.is_valid_number(parse_phone)
+print("        Is Valid:", is_valid_no)
+print("-------------------------------------")
